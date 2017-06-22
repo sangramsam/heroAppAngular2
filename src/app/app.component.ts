b313192541edd9565baedd1b250f3e9050aa535e
sangram singh <singh.sangram56@gmail.com>
2017-06-22 11:34:11 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-}
+  public font_size="12px";
+  public background_color="grey ";
+  randoncolor=this.getRandomColor();
+  public getRandomColor() {
+      var letters = '0123456789ABCDEF'.split('');
+      var color = '#';
+      for (var i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+    setColor() {
+    this.randoncolor=this.getRandomColor();
+    }
+  }
+
